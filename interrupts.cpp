391fc54898742c70066a11cc01b7757480cb6219
Maathusan Sathiendran <155667928+maat2405@users.noreply.github.com>
2025-11-07 17:34:37 -0500

    Fixed timing bug

--- interrupts.cpp
+++ interrupts.cpp
@@ -9,16 +9,17 @@
 
 #include<interrupts.hpp>
 
+// Global PID counter
 static unsigned int next_pid = 1;
+
 std::tuple<std::string, std::string, int> simulate_trace(std::vector<std::string> trace_file, int time, std::vector<std::string> vectors, std::vector<int> delays, std::vector<external_file> external_files, PCB current, std::vector<PCB> wait_queue) {
 
     std::string trace;      //!< string to store single line of trace file
     std::string execution = "";  //!< string to accumulate the execution output
     std::string system_status = "";  //!< string to accumulate the system status output
-    int simulation_time = 0; // this is the total duration of the sim
+    int current_time = time; // Use the passed time parameter consistently
     const int context_time = 10; // context
     const int isr_time = 200; // isr
-    int current_time = time;
     
 
     //parse each line of the input trace file. 'for' loop to keep track of indices.
@@ -28,44 +29,44 @@ std::tuple<std::string, std::string, int> simulate_trace(std::vector<std::string
         auto [activity, duration_intr, program_name] = parse_trace(trace);
 
         if (activity == "CPU") {
-            execution += std::to_string(simulation_time) + ", " + std::to_string(duration_intr) + ", CPU Burst\n";
-            simulation_time += duration_intr;
+            execution += std::to_string(current_time) + ", " + std::to_string(duration_intr) + ", CPU Burst\n";
+            current_time += duration_intr;
         }
 
         else if (activity == "SYSCALL") {
-            auto [execution_temp, simulation_time_temp] = intr_boilerplate(simulation_time, duration_intr, context_time, vectors);
+            auto [execution_temp, simulation_time_temp] = intr_boilerplate(current_time, duration_intr, context_time, vectors);
             execution += execution_temp;
-            simulation_time = simulation_time_temp;
+            current_time = simulation_time_temp;
 
-            execution += std::to_string(simulation_time) + ", " + std::to_string(delays[duration_intr]) + ", " + "SYSCALL: run the ISR for device " + std::to_string(duration_intr) + "\n";
-            simulation_time += delays[duration_intr];
+            execution += std::to_string(current_time) + ", " + std::to_string(delays[duration_intr]) + ", " + "SYSCALL: run the ISR for device " + std::to_string(duration_intr) + "\n";
+            current_time += delays[duration_intr];
 
-            execution += std::to_string(simulation_time) + ", " + std::to_string(isr_time) + ", " + "transfer data from device to memory\n";
-            simulation_time += isr_time;
+            execution += std::to_string(current_time) + ", " + std::to_string(isr_time) + ", " + "transfer data from device to memory\n";
+            current_time += isr_time;
 
-            execution += std::to_string(simulation_time) + ", " + std::to_string(isr_time) + ", " + "check for errors\n";
-            simulation_time += isr_time;
+            execution += std::to_string(current_time) + ", " + std::to_string(isr_time) + ", " + "check for errors\n";
+            current_time += isr_time;
 
-            execution += std::to_string(simulation_time) + ", " + std::to_string(1) + ", " + "IRET\n";
-            simulation_time++;
+            execution += std::to_string(current_time) + ", " + std::to_string(1) + ", " + "IRET\n";
+            current_time++;
 
         }
 
         else if (activity == "END_IO") {
-            auto [execution_temp, simulation_time_temp] = intr_boilerplate(simulation_time, duration_intr, context_time, vectors);
+            auto [execution_temp, simulation_time_temp] = intr_boilerplate(current_time, duration_intr, context_time, vectors);
             execution += execution_temp;
-            simulation_time = simulation_time_temp;
+            current_time = simulation_time_temp;
 
-            execution += std::to_string(simulation_time) + ", " + std::to_string(delays[duration_intr]) + ", " + "END I/O\n";
-            simulation_time += delays[duration_intr];
+            execution += std::to_string(current_time) + ", " + std::to_string(delays[duration_intr]) + ", " + "END I/O\n";
+            current_time += delays[duration_intr];
 
-            execution += std::to_string(simulation_time) + ", " + std::to_string(1) + ", " + "IRET\n";
-            simulation_time++;
+            execution += std::to_string(current_time) + ", " + std::to_string(1) + ", " + "IRET\n";
+            current_time++;
         }
 
         else if (activity == "UNKOWN_ACTIVITY") {
-            execution += std::to_string(simulation_time) + ", " + std::to_string(1) + ", " + "Unknown Activity\n";
-            simulation_time++;
+            execution += std::to_string(current_time) + ", " + std::to_string(1) + ", " + "Unknown Activity\n";
+            current_time++;
         } else if(activity == "FORK") {
             auto [intr, time] = intr_boilerplate(current_time, 2, 10, vectors);
             execution += intr;
@@ -73,7 +74,7 @@ std::tuple<std::string, std::string, int> simulate_trace(std::vector<std::string
 
             ///////////////////////////////////////////////////////////////////////////////////////////
             //Add your FORK output here
-
+            
             // Clone the PCB for the child process
             execution += std::to_string(current_time) + ", " + std::to_string(duration_intr) + ", cloning the PCB\n";
             current_time += duration_intr;
@@ -91,8 +92,7 @@ std::tuple<std::string, std::string, int> simulate_trace(std::vector<std::string
             
             // Scheduler called
             execution += std::to_string(current_time) + ", " + std::to_string(0) + ", scheduler called\n";
-
-
+            
             ///////////////////////////////////////////////////////////////////////////////////////////
 
             //The following loop helps you do 2 things:
